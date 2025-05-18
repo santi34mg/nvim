7e941a8fcfc612be5c1f066080759fb2e231403c
Santiago Silvera <santiagosil3434@gmail.com>
2025-05-18 01:15:53 -0300

    Mason integration

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "mason-org/mason.nvim",
+    config = function()
+      require "mason".setup()
+    end
+  },
+
+  {
     "neovim/nvim-lspconfig",
     dependencies = {
       {
@@ -13,28 +20,6 @@ return {
       },
     },
     config = function()
-      require('lspconfig').lua_ls.setup {}
-      require('lspconfig').ts_ls.setup {
-        on_attach = function(client, bufnr)
-          vim.api.nvim_buf_set_option(bufnr, "expandtab", true)
-          vim.api.nvim_buf_set_option(bufnr, "shiftwidth", 4)
-          vim.api.nvim_buf_set_option(bufnr, "tabstop", 4)
-        end,
-      }
-      require('lspconfig').clangd.setup {}
-      -- require('lspconfig').pylyzer.setup {}
-      require('lspconfig').pylsp.setup {}
-      require('lspconfig').rust_analyzer.setup {}
-      require('lspconfig').gopls.setup {
-        on_attach = function(client, bufnr)
-          vim.api.nvim_buf_set_option(bufnr, "expandtab", true)
-          vim.api.nvim_buf_set_option(bufnr, "shiftwidth", 4)
-          vim.api.nvim_buf_set_option(bufnr, "tabstop", 4)
-        end,
-      }
-      require('lspconfig').ocamllsp.setup {}
-      require('lspconfig').html.setup {}
-
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
           print("LspAttach")
@@ -51,5 +36,14 @@ return {
         end,
       })
     end,
-  }
+  },
+
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require "mason-lspconfig".setup {
+        ensure_installed = { "lua_ls", "pylsp" },
+      }
+    end
+  },
 }

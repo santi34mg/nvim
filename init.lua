a048d2789648c0587896dbfb7a346a0d37948d4c
santi34mg <ssilvera@correo.um.edu.uy>
2026-04-18 00:09:53 -0300

    opencode

--- init.lua
+++ init.lua
@@ -35,8 +35,47 @@ vim.pack.add({
     { src = "https://github.com/vague2k/vague.nvim" },
     { src = "https://github.com/folke/trouble.nvim" },
 })
+vim.pack.add({
+    { src = "https://github.com/nickjvandyke/opencode.nvim" },
+    { src = "https://github.com/folke/snacks.nvim" },
+})
 
-vim.cmd("colorscheme vague")
+-- Setup snacks.nvim (if installed)
+local ok, snacks = pcall(require, "snacks")
+if ok then
+  snacks.setup({
+    input = {},
+    picker = {
+      actions = {
+        opencode_send = function(...) return require("opencode").snacks_picker_send(...) end,
+      },
+      win = {
+        input = {
+          keys = {
+            ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+          },
+        },
+      },
+    },
+  })
+end
+
+-- opencode.nvim config
+---@type opencode.Opts
+vim.g.opencode_opts = {}
+vim.o.autoread = true
+
+vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
+vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
+vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
+vim.keymap.set({ "n", "x" }, "go",    function() return require("opencode").operator("@this ") end,         { desc = "Add range to opencode", expr = true })
+vim.keymap.set("n",          "goo",   function() return require("opencode").operator("@this ") .. "_" end,  { desc = "Add line to opencode", expr = true })
+vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll opencode up" })
+vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "Scroll opencode down" })
+vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
+vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
+
+vim.cmd("colorscheme mintheme")
 vim.cmd("set completeopt+=noselect")
 vim.cmd(":hi statusline guibg=NONE")
 
@@ -75,7 +114,7 @@ vim.lsp.config("fiber", {
 })
 
 -- lsp
-vim.lsp.enable({ "lua_ls", "rust_analyzer", "pyright", "ts_ls", "clangd", "zls", "gopls", "fiber" })
+vim.lsp.enable({ "lua_ls", "rust_analyzer", "ruff", "pyright", "ts_ls", "clangd", "zls", "gopls", "html" })
 
 vim.keymap.set("n", "<leader>ah", function()
     local dir  = vim.fn.expand("%:p:h") -- current file's directory
